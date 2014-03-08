b53a19e1830aaf9a01d805b4c7057865e87b1194
arpit.gautam <arpit.gautam@outlook.com>
2014-03-09 02:55:11 +0530

    Commit

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,6 @@
-var mod = require('./FBUserFetch');
-var communicatorModule = require('./FBGraphAPICommunicator');
-
-var token = 'CAACEdEose0cBAEf84oZAVlL1CorTmUvesCLutCfSzjSjZAmGZBgg0yZC9agOK7m9bCb6SgTc1zVh1VY6LkeVhrIik0dexbkm2FRpswJwHLZCesnab7sX5829kSjhnTqBUSNZC6rm7VqXIxBqHH7JZA2Wb9RoowD17YpoHExBhZCULTtyptbwBR8CZBabwHZAJgglw0hwbAFU03yQZDZD';
+var mod = require('./FBUserFetch'),
+communicatorModule = require('./FBGraphAPICommunicator'),
+token = '';
 
 function success(res) {
 	var object = JSON.parse(res.toString());
