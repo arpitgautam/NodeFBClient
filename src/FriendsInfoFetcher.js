f3eae6d00f83e6ef816c495047d410562b685c87
arpit.gautam <arpit.gautam@outlook.com>
2014-03-18 15:16:39 +0530

    passing this to promsie callbacks

--- src/FriendsInfoFetcher.js
+++ src/FriendsInfoFetcher.js
@@ -4,7 +4,8 @@ communicatorModule = require('./FBGraphAPICommunicator'),
 userFetch = require('./UserFetch');
 
 
-var self;
+//Caution - this class is unsafe for creating multiple objects in same context
+//do not attempt it
 function FriendsInfoFetcher(){
     self = this;
     this.nameMap = {};
@@ -22,17 +23,16 @@ FriendsInfoFetcher.prototype.setCommunicator = function (comm) {
 FriendsInfoFetcher.prototype.fetch = function(onSuccess, onError) {
 
 	var sendPromise = this._communicator.send();
-	sendPromise.then(this._sendIndividualRequests).then(this._createResponse).
+	sendPromise.then(this._sendIndividualRequests.bind(this)).then(this._createResponse.bind(this)).
 						then(onSuccess,onError).done();
 	
 };
-
 
 FriendsInfoFetcher.prototype._sendIndividualRequests = function (data) {
 	var promises = new Array();
     var dataObject = JSON.parse(data);
     for (var i  in dataObject.data) {
-    	var comm = new communicatorModule.FaceBookGraphAPICommunicator(self._communicator.token);
+    	var comm = new communicatorModule.FaceBookGraphAPICommunicator(this._communicator.token);
     	var id = dataObject.data[i].id;
         var name = dataObject.data[i].name;
         comm.setPath('/' + id + '?fields=picture');
@@ -49,7 +49,7 @@ FriendsInfoFetcher.prototype._createResponse = function (data) {
     for (var i in data) {
         var response = data[i].value;
         var dataObject = JSON.parse(response);
-        var name = self.nameMap[dataObject.id]
+        var name = this.nameMap[dataObject.id]
         var pictureURL = dataObject.picture.data.url;
         result.data.push({
             "name": name,
