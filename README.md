39ad10d068727ba1f673512f453147478bc18eb0
arpit.gautam <arpit.gautam@outlook.com>
2014-03-25 21:53:57 +0530

    log4s usage in...

--- README.md
+++ README.md
@@ -3,30 +3,47 @@ A library for making facebook graph API requests easy
 
 
 ## Usage
-    //token is fb user token
+
+    var fb = require('./fbclient'),
+    log4js  = require('log4js'),
+    logModule = require('./lib/logger');
+
+ 
+
+    (function initLogger() {
+        log4js.loadAppender('file');
+        log4js.addAppender(log4js.appenders.file('../logs/cheese.log'), 'fbClient');
+        var logger = log4js.getLogger('fbClient');
+        logger.setLevel('DEBUG');
+        logModule.setLogger(logger);
+    })();
+
+    var token = 'INSERT VALID token here';
     fb.fbclient.friendInfo(token, function (data) {
+	    //console.log(data);
+        //if has next
         if(fb.fbclient.hasNext()){
-            nextCall();
-        }
+    	    nextCall();
+	    }
 
     }, function (e) {
+        console.log('This is an error');
         console.log(e);
     });
 
 
-
-    //this is calling nextCall recursive unless no more friends are there
     function nextCall(){
-        fb.fbclient.next(token, function (data) {
+        fb.fbclient.next(token, function (data) {    
+       // console.log(data);
         if(fb.fbclient.hasNext()){
 		    nextCall();
         }
 
     }, function (e) {
-	    console.log(e);
+	    console.log('This is another error');
+        console.log(e);
     });
     }
-
 
 ## Developing
 
--- src/lib/FriendsInfoFetcher.js
+++ src/lib/FriendsInfoFetcher.js
@@ -6,9 +6,14 @@ logger = require('./logger');
 
 //Caution - this class is unsafe for creating multiple objects in same context
 //do not attempt it
+var counter = 0;
 function FriendsInfoFetcher(){
     self = this;
     this.nameMap = {};
+    result = {"data": []};
+    this.getResult = function () {
+        return result;
+    }
 };
 
 util.inherits(FriendsInfoFetcher, userFetch.UserFetch);
@@ -21,60 +26,69 @@ FriendsInfoFetcher.prototype.setCommunicator = function (comm) {
 FriendsInfoFetcher.prototype.fetch = function (onSuccess, onError) {
 
     var sendPromise = this._communicator.send();
-    logger.log('debug', 'fetching friends for active user');
-    sendPromise.then(this._sendIndividualRequests.bind(this)).then(this._createResponse.bind(this)).
+    logger.log('trace', 'fetching friends for active user');
+    sendPromise.then(this._sendIndividualRequests.bind(this)).then(this._aggregateResponse.bind(this)).
 						then(onSuccess, onError).done();
 
 };
+
 FriendsInfoFetcher.prototype.fetchNext = function (onSuccess, onError) {
     this._communicator.setPath(this._communicator.getNextURL());
     var sendPromise = this._communicator.send();
-    logger.log('debug', 'fetching next friends for active user');
-    sendPromise.then(this._sendIndividualRequests.bind(this)).then(this._createResponse.bind(this)).
-						then(onSuccess, onError).done();
+    logger.log('trace', 'fetching next friends for active user');
+    logger.log('debug', 'sending request for:' + this._communicator.getNextURL());
+    sendPromise.then(this._sendIndividualRequests.bind(this)).then(this._aggregateResponse.bind(this)).
+                        then(onSuccess, onError).done();
 
 };
 
 FriendsInfoFetcher.prototype._sendIndividualRequests = function (data) {
-	var promises = new Array();
+    var promises = new Array();
     logger.log('debug', 'recieved data for friend list:' + data);
-        
+     logger.log('trace', 'in send individual requests');
     var dataObject = JSON.parse(data);
-    logger.log('debug', 'fetching friends for active user');
-    for (var i  in dataObject.data) {
-    	var comm = new communicatorModule.FaceBookGraphAPICommunicator(this._communicator.getToken());
-    	var id = dataObject.data[i].id;
+    logger.log('trace', 'fetching individual friends for active user');
+    for (var i in dataObject.data) {
+        //TODO- make this tick
+        var comm = new communicatorModule.FaceBookGraphAPICommunicator(this._communicator.getToken());
+        var id = dataObject.data[i].id;
         var name = dataObject.data[i].name;
         var path = '/' + id + '?fields=picture';
         comm.setPath(path);
         logger.log('debug', 'sending request to:' + path);
         self.nameMap[id] = name;
-        promises[i] =  comm.send();
+        promises[i] = comm.send();
+        promises[i].then(this._createResponse.bind(this));
     }
     return Q.allSettled(promises);
 }
 
+//result.data is getting updated here
+// do not try to attempt modify it in a different function
 FriendsInfoFetcher.prototype._createResponse = function (data) {
+    logger.log('trace', 'in create response');
+    logger.log('debug', 'concating individual response');
+    logger.log('debug', 'data:' + data);
+    var dataObject = JSON.parse(data);
+    var name = this.nameMap[dataObject.id]
+    var pictureURL = dataObject.picture.data.url;
+    this.getResult().data.push({
+        "name": name,
+        "picture": pictureURL,
+        "id": dataObject.id
+    });
+    counter = counter + 1;
+}
 
-    var result = {"data": []};
-    logger.log('debug', 'concating responses');
-   
-    //this loop can take a lot of time,
-    // need refactoring
-    for (var i in data) {
-        var response = data[i].value;
-        logger.log('debug', 'data:' + response);
-        var dataObject = JSON.parse(response);
-        var name = this.nameMap[dataObject.id]
-        var pictureURL = dataObject.picture.data.url;
-        result.data.push({
-            "name": name,
-            "picture": pictureURL,
-            "id" : dataObject.id
-        });
-
+FriendsInfoFetcher.prototype._aggregateResponse = function (data) {
+    logger.log('trace','in aggreate Response');
+    if (this.getResult() == null) {
+        logger.log('error', 'result aggregated by friend fetcher is null');
+        throw "No data to return";
     }
-    return JSON.stringify(result);
+    var stringResponse = JSON.stringify(this.getResult())
+    logger.log('debug', 'concatenated response' + stringResponse);
+    return stringResponse;
 }
 
 module.exports.FriendsInfoFetcher = FriendsInfoFetcher;
